ffe4a3ecfd6e5e29277b7ab827ef049df1e251b7
Anthony ROUSSEL <anthony@roussel.dev>
2024-11-11 23:28:59 +0100

    [workflow] add openstack_workflow_workflow_v2 resource

--- openstack/data_source_openstack_workflow_workflow_v2_test.go
+++ openstack/data_source_openstack_workflow_workflow_v2_test.go
@@ -1,42 +1,31 @@
 package openstack
 
 import (
-	"context"
-	"os"
-	"strings"
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-
-	"github.com/gophercloud/gophercloud/v2/openstack/workflow/v2/workflows"
 )
 
 func TestAccWorkflowV2WorkflowDataSource_basic(t *testing.T) {
-	var workflowID string
-
-	if os.Getenv("TF_ACC") != "" {
-		workflow, err := testAccWorkflowV2WorkflowCreate()
-		if err != nil {
-			t.Fatal(err)
-		}
-		workflowID = workflow.ID
-		defer testAccWorkflowV2WorkflowDelete(workflow.ID) //nolint:errcheck
-	}
-
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheckWorkflow(t)
 			testAccPreCheckNonAdminOnly(t)
 		},
 		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckWorkflowV2WorkflowDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccWorkflowV2WorkflowDataSourceBasic,
+			},
+			{
+				Config: testAccWorkflowV2WorkflowDataSourceSource(),
 				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(
+						"data.openstack_workflow_workflow_v2.workflow_1", "id"),
 					resource.TestCheckResourceAttr(
-						"data.openstack_workflow_workflow_v2.workflow_1", "id", workflowID),
-					resource.TestCheckResourceAttr(
-						"data.openstack_workflow_workflow_v2.workflow_1", "name", "my_workflow"),
+						"data.openstack_workflow_workflow_v2.workflow_1", "name", "my_workflow_datasource"),
 					resource.TestCheckResourceAttr(
 						"data.openstack_workflow_workflow_v2.workflow_1", "namespace", "my_namespace"),
 					resource.TestCheckResourceAttr(
@@ -57,77 +46,40 @@ func TestAccWorkflowV2WorkflowDataSource_basic(t *testing.T) {
 	})
 }
 
-func testAccWorkflowV2WorkflowDelete(workflowID string) error {
-	config, err := testAccAuthFromEnv()
-	if err != nil {
-		return err
-	}
-
-	client, err := config.WorkflowV2Client(context.TODO(), osRegionName)
-	if err != nil {
-		return err
-	}
-
-	err = workflows.Delete(context.TODO(), client, workflowID).ExtractErr()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func testAccWorkflowV2WorkflowCreate() (*workflows.Workflow, error) {
-	config, err := testAccAuthFromEnv()
-	if err != nil {
-		return nil, err
-	}
-
-	client, err := config.WorkflowV2Client(context.TODO(), osRegionName)
-	if err != nil {
-		return nil, err
-	}
-
-	createWorkflowOpts := workflows.CreateOpts{
-		Scope:      "private",
-		Namespace:  "my_namespace",
-		Definition: strings.NewReader(testAccWorkflowV2WorkflowDataSourceBasicDefinition),
-	}
-
-	workflows, err := workflows.Create(context.TODO(), client, createWorkflowOpts).Extract()
-	if err != nil {
-		return nil, err
-	}
-
-	workflow := workflows[len(workflows)-1]
-
-	return &workflow, nil
-}
-
 const testAccWorkflowV2WorkflowDataSourceBasic = `
-data "openstack_workflow_workflow_v2" "workflow_1" {
-	name      = "my_workflow"
+resource "openstack_workflow_workflow_v2" "workflow_1" {
 	namespace = "my_namespace"
+	scope     = "private"
+	definition = <<EOF
+    version: '2.0'
+
+    my_workflow_datasource:
+      description: Simple echo example
+
+      input:
+        - my_arg1
+        - my_arg2
+
+      tags:
+        - echo
+
+      tasks:
+        echo:
+          action: std.echo
+          input:
+            output:
+              my_arg1: <% $.my_arg1 %>
+              my_arg2: <% $.my_arg2 %>
+	EOF
 }
 `
 
-const testAccWorkflowV2WorkflowDataSourceBasicDefinition = `
-version: '2.0'
-
-my_workflow:
-  description: Simple echo example
-
-  input:
-    - my_arg1
-    - my_arg2
-
-  tags:
-    - echo
-
-  tasks:
-    echo:
-      action: std.echo
-      input:
-        output:
-          my_arg1: <% $.my_arg1 %>
-          my_arg2: <% $.my_arg2 %>
-`
+func testAccWorkflowV2WorkflowDataSourceSource() string {
+	return fmt.Sprintf(`
+%s
+data "openstack_workflow_workflow_v2" "workflow_1" {
+	name      = openstack_workflow_workflow_v2.workflow_1.name
+	namespace = openstack_workflow_workflow_v2.workflow_1.namespace
+}
+`, testAccWorkflowV2WorkflowDataSourceBasic)
+}
